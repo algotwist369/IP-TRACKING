4dd1159e5b3f716472ea53c38e509a2c7a33fbc0
Ankit pathak <info.algotwist@gmail.com>
2025-08-27 19:44:14 +0530

    monor change

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
 module.exports = {
     MONGO_URI: "mongodb+srv://infoalgotwist:b78TEY60PiRWYoS9@cluster0.kidgbtf.mongodb.net/traffic-monitor",
     PORT: 3080,
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 76a1aac14591b0ecb4985562f61c88adb5552642
